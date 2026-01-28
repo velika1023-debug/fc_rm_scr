645f4ef9ae2ebc8238e4e9ebb75cdab77f9625cb
velika1023-debug <velika1023@gmail.com>
2026-01-28 16:54:56 +0900

    Add files via upload

---
+++ mpc.tcl
@@ -0,0 +1,19 @@
+##########################################################################################
+# Script: mpc.tcl
+# Version: V-2023.12-SP4
+# Copyright (C) 2014-2024 Synopsys, Inc. All rights reserved.
+##########################################################################################
+
+## -----------------------------------------------------------------------------
+## Description:
+## This is a basic Minimum Physical Constraint (MPC) file for providing some
+## physical guidance until a more complete floorplan exists.
+## It defaults to auto floorplan but users can modify and 
+## enhance the physical constraints provided.
+## -----------------------------------------------------------------------------
+
+set_app_options -name compile.auto_floorplan.enable -value true
+
+## -----------------------------------------------------------------------------
+## End of File
+## -----------------------------------------------------------------------------
